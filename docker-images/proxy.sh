9f66f82a0385ad679b3da1a1f39e4db60d76538d
Bonga Masangu <bonga.masangu@bonga.masangu@partner.bmw.co.za>
2020-03-09 19:20:34 +0200

    changed values

--- docker-images/proxy.sh
+++ docker-images/proxy.sh
@@ -4,5 +4,5 @@ if ping -c 1 ix.de ; then
     echo "direct internet doing nothing"
 else
     echo "proxy environment detected setting proxy"
-    export http_proxy=http://qxz0hv8:zb0ngerBMWgermany4@proxy.w9:80
+    export http_proxy=http://user:password@proxy:8080
 fi
